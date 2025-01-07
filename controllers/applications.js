6a2375dd8c89e4664a07ca790c6180221135606b
Cesar Preza <cpreza93@yahoo.com>
2025-01-06 20:50:08 -0800

    refactored routes

--- controllers/applications.js
+++ controllers/applications.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const User = require('../models/user.js');
 
 // we will build out our router logic here
+//INDEX
 router.get('/', async (req, res) => {
     try {
       // Look up the user from req.session
@@ -22,47 +23,12 @@ router.get('/', async (req, res) => {
     }
 });
 
+//NEW
 router.get('/new', async (req, res) => {
     res.render('applications/new.ejs');
 });
 
-// controllers/applications.js`
-
-router.post('/', async (req, res) => {
-    try {
-      // Look up the user from req.session
-        const currentUser = await User.findById(req.session.user._id);
-        // Push req.body (the new form data object) to the
-        // applications array of the current user
-        currentUser.applications.push(req.body);
-        // Save changes to the user
-        await currentUser.save();
-        // Redirect back to the applications index view
-        res.redirect(`/users/${currentUser._id}/applications`);
-    } catch (error) {
-        // If any errors, log them and redirect back home
-        console.log(error);
-        res.redirect('/');
-    }
-});
-
-router.get('/:applicationId', async (req, res) => {
-    try {
-    // Look up the user from req.session
-    const currentUser = await User.findById(req.session.user._id);
-    // Find the application by the applicationId supplied from req.params
-    const application = currentUser.applications.id(req.params.applicationId);
-    // Render the show view, passing the application data in the context object
-    res.render('applications/show.ejs', {
-        application: application,
-    });
-    } catch (error) {
-    // If any errors, log them and redirect back home
-    console.log(error);
-    res.redirect('/');
-    }
-});
-
+//DELETE
 router.delete('/:applicationId', async (req, res) => {
     try {
         // Look up the user from req.session
@@ -81,6 +47,47 @@ router.delete('/:applicationId', async (req, res) => {
     }
 });
 
+// controllers/applications.js`
+
+//CREATE
+router.post('/', async (req, res) => {
+    try {
+      // Look up the user from req.session
+        const currentUser = await User.findById(req.session.user._id);
+        // Push req.body (the new form data object) to the
+        // applications array of the current user
+        currentUser.applications.push(req.body);
+        // Save changes to the user
+        await currentUser.save();
+        // Redirect back to the applications index view
+        res.redirect(`/users/${currentUser._id}/applications`);
+    } catch (error) {
+        // If any errors, log them and redirect back home
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+//SHOW
+router.get('/:applicationId', async (req, res) => {
+    try {
+    // Look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Find the application by the applicationId supplied from req.params
+    const application = currentUser.applications.id(req.params.applicationId);
+    // Render the show view, passing the application data in the context object
+    res.render('applications/show.ejs', {
+        application: application,
+    });
+    } catch (error) {
+    // If any errors, log them and redirect back home
+    console.log(error);
+    res.redirect('/');
+    }
+});
+
+
+//UPDATE
 router.put('/:applicationId', async (req, res) => {
     try {
         // Find the user from req.session
@@ -103,6 +110,7 @@ router.put('/:applicationId', async (req, res) => {
     }
 });
 
+//EDIT
 router.get('/:applicationId/edit', async (req, res) => {
     try {
         const currentUser = await User.findById(req.session.user._id);
